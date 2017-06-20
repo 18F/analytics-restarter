146daec2a425c033578f4d384e4123bd84e36615
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-06-20 08:56:01 -0500

    Move restarter invocation in config.ru
    This commit moves the restarter invocation to the config.ru so that it
    will be started when the app is started. Prior to this commit, the
    `restart` function was called when the app file was required.

--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
 require "./lib/app"
+require "./lib/restarter"
+
+# Start restarting builds
+Restarter.restart
 
 Rack::Handler.default.run Application.new, Port: ENV["PORT"]
--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,4 @@
 require "sinatra"
-require_relative "./restarter"
-
-# Start restarting builds
-Restarter.restart
 
 # An app to render a 200 for New Relic monitoring
 class Application < Sinatra::Base
